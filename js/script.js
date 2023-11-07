253f692228b3a64fb56ccefbde7c816ad6c6f3bf
fcarrara24 <f.carrara24@studenti.unibg.it>
2023-11-07 15:10:13 +0100

    project config

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const { createApp } = Vue;
+
+
+const obj = {
+    data() {
+        return {
+            message: 'hello vue'
+        }
+    },
+    methods: {
+        changeMessage() {
+            this.message = 'ciao';
+        }
+    }
+}
+
+createApp(obj).mount('#app')
