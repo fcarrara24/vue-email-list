aa7380bb40395dc17032a2f5a5a8617219bb2622
fcarrara24 <f.carrara24@studenti.unibg.it>
2023-11-07 15:26:08 +0100

    got response from website

--- js/script.js
+++ js/script.js
@@ -4,14 +4,39 @@ const { createApp } = Vue;
 const obj = {
     data() {
         return {
-            message: 'hello vue'
+            mailArray: [],
+            APIwebsite: `https://flynn.boolean.careers/exercises/api/`,
+            section: `random/mail`
         }
     },
     methods: {
-        changeMessage() {
-            this.message = 'ciao';
+        createArray() {
+            for (let index = 0; index < 10; index++) {
+                this.mailArray.push(this.getEmail())
+            }
+
+        },
+        getEmail() {
+            axios
+                .get(`${this.APIwebsite}${this.section}`)
+                .then(
+                    (res) => {
+                        console.log(res.data.response)
+                        return res
+                    }
+                )
+            //  
+        }
+    },
+    computed: {
+        arrayLength() {
+            return this.mailArray.length
         }
     }
 }
 
 createApp(obj).mount('#app')
+
+
+
+// https://flynn.boolean.careers/exercises/api/random/mail
